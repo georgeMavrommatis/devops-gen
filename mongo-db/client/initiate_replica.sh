fd1c57626efdce8bcfed8794e8abed81331a7bd0
Georgios Mavrommatis <george.mavrommatis@generali.gr>
2022-12-01 20:13:59 +0200

    fixed arbiter and initiate_replica.sh now can locate the primary

--- mongo-db/client/initiate_replica.sh
+++ mongo-db/client/initiate_replica.sh
@@ -1,30 +1,86 @@
 #!/bin/bash
 
 echo "Starting replica set initialize"
-#docker run --rm -it mongo:5 mongo --port 30001 --host sw-swarm-prd-mgr03
-#until mongo --port 30001 --host sw-swarm-prd-mgr03 --eval "print(\"waited for connection\")"
-until mongo --host mongodb1 --eval "print(\"waited for connection\")"
-do
-    sleep 2
-done
 
-echo "Connection finished"
+var=0
+until mongosh --host mongodb1 --eval "print(\"waited for connection\")"; do
+  sleep 2
+  var=$((var + 1))
+  echo "var =" $var
+  if [[ var -eq 10 ]]; then
+    echo "-->Error!!! Unable to connect to mongodb1 via overlay Newtork!!!<--"
+    exit 0
+  fi
+done
+echo "-->Connected to mongodb1<--"
+
+var=0
+until mongosh --host mongodb2 --eval "print(\"waited for connection\")"; do
+  sleep 2
+  var=$((var + 1))
+  echo "var =" $var
+  if [[ var -eq 10 ]]; then
+    echo "-->Error!!! Unable to connect to mongodb2 via overlay Newtork!!!<--"
+    exit 0
+  fi
+done
+echo "-->Connected to mongodb2<--"
+
+var=0
+until mongosh --host mongodb_arbiter --eval "print(\"waited for connection\")"; do
+  sleep 2
+  var=$((var + 1))
+  echo "var =" $var
+  if [[ var -eq 10 ]]; then
+    echo "-->Error!!! Unable to connect to mongodb_arbiter via overlay Newtork!!!<--"
+    exit 0
+  fi
+done
+echo "-->Connected to mongodb_arbiter<--"
+echo "-->Connection finished<--"
+
+#fetch response of who is primary
+request=$(
+  mongo --host mongodb1 <<EOF
+rs.isMaster().primary
+EOF
+)
+
+#remove new line characters
+request=${request//$'\n'/}
+
+echo "Locate Primary Server via connection -->"
+echo $request
+echo "<--"
+
+if [[ $request == *"mongodb1:27017bye"* ]]; then
+  primaryServer="mongodb1"
+elif [[ $request == *"mongodb2:27017bye"* ]]; then
+  primaryServer="mongodb2"
+else
+  echo "Error!!! no Primary Server detected!!!"
+  exit 0
+fi
+
+#Print message based on the value of $passed
+echo "Primary Server: " $primaryServer
+
 echo "Creating replica set"
-mongo --host mongodb1 <<EOF
+mongo --host $primaryServer <<EOF
 rs.initiate(
   {
     _id : 'rs0',
     members: [
       { _id : 0, host : "mongodb1:27017" },
       { _id : 1, host : "mongodb2:27017" },
-      { _id : 2, host : "mongo_arbiter:27017", arbiterOnly: true }
+      { _id : 2, host : "mongodb_arbiter:27017", arbiterOnly: true }
     ]
   }
 )
 EOF
-echo "----------> replica set created <----------"
+echo "----------> Replica set created in "$primaryServer"<----------"
 
-mongo --host mongodb1 <<EOF
-rs.addArb("mongo_arbiter:27017")
+mongo --host $primaryServer <<EOF
+rs.addArb("mongodb_arbiter:27017")
 EOF
-echo "----------> Arbiter Added <----------"
+echo "----------> Added Arbiter in "$primaryServer" <----------"
